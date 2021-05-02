8adb6cab03070d1707386cbeb9b11fd2123c9d78
agumy <nov13sh@gmail.com>
2021-05-02 15:14:18 +0900

    object and class

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -210,3 +210,135 @@ print(mappedNumbers)
 
 let sortedNumbers = numbers.sorted { $0 > $1 }
 print(sortedNumbers)
+
+class Shape {
+    var numberOfSides: Int = 0
+    var name: String
+    
+    init (name: String) {
+        self.name = name
+    }
+    
+    func simpleDescription() -> String {
+        return "A Shape with \(numberOfSides) sides."
+    }
+    
+    let TAX = 1.1
+    func withTax(amount: Double) -> Double {
+        return TAX * amount
+    }
+}
+
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+    
+    init (name: String) {
+        self.name = name
+    }
+    
+    func simpleDescription() -> String {
+        return "A Shape with \(numberOfSides) sides."
+    }
+}
+
+var shape = Shape(name: "John")
+shape.numberOfSides = 7
+var shpaeDescription = shape.simpleDescription()
+
+class Square: NamedShape {
+    var sideLength: Double
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)"
+    }
+}
+
+let test = Square(sideLength: 5.2, name: "my test square")
+test.area()
+test.simpleDescription()
+
+class Circle: NamedShape {
+    var radius: Double
+    let Pi = 3.14
+    
+    init(radius: Double, name: String) {
+        self.radius = radius
+        super.init(name: name)
+    }
+    
+    func area() -> Double {
+        return radius * radius * Pi
+    }
+}
+
+
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+    
+    override func simpleDescription() -> String {
+        return "An rquilateral triangle with sides of length \(sideLength)"
+    }
+}
+
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+print(triangle.perimeter)
+
+triangle.perimeter = 9.9
+print(triangle.perimeter)
+
+class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+    }
+    
+    var square: Square {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+
+var triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
+
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+
+triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+print(triangleAndSquare.triangle.sideLength)
+
+let optionalSquare: Square? = Square(sideLength: 2.5, name: "optional square")
+
+let sideLength = optionalSquare?.sideLength
