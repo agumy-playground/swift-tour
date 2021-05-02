ee715b708522d9842d0f13caf540e6bf969ad8bd
agumy <nov13sh@gmail.com>
2021-05-02 14:15:51 +0900

    wip for swift tour

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,102 @@
 let explicitFloat: Float = 5;
 
 print(explicitFloat)
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples)."
+let name = "shave"
+let greeting = "I'm \(name)"
+
+let quotation = """
+I said "I have \(apples) apples."
+And then I said "I have \(apples + oranges) pieces of fruit."
+"""
+
+var shoppingList = ["catfish", "water", "tulips"]
+shoppingList[1] = "bottle of water"
+
+var occupations = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic"
+]
+
+occupations["Jayne"] = "Public Relations"
+
+shoppingList.append("blue paint")
+print(shoppingList)
+
+let emptyArray = [String]()
+let emptyDictionary = [String: Float]()
+
+shoppingList = []
+occupations = [:]
+
+
+let inidivisualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+
+for score in inidivisualScores {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+
+print(teamScore)
+
+var optionalString: String? = "Hello"
+print(optionalString == nil)
+
+var optionalName: String? = nil
+var greeting2 = "Hello!"
+
+if let name = optionalName {
+    greeting2 = "Hello, \(name)"
+} else {
+    greeting2 = "Who are you?"
+}
+
+print(greeting2)
+
+let nickname: String? = nil
+let fullName: String = "John Appleseed"
+let informalGreeting = "Hi \(nickname ?? fullName)"
+
+print(informalGreeting)
+
+
+let vegetable = "red pepper"
+switch vegetable {
+    case "celery":
+        print("Add some raisins and make ants on a log.")
+    case "cucumber", "watercress":
+        print("That would make a good tea sandwich.")
+    case let x where x.hasSuffix("pepper"):
+        print("Is it a spicy \(x)?")
+    default:
+        print("Everything tastes good in soup.")
+}
+
+let interstingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25]
+]
+var largest = 0
+var largestKey = ""
+for(key, numbers) in interstingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+            largestKey = key
+        }
+    }
+}
+
+print(largest, largestKey)
