cb1793cd752cea24fac944b637e0c2c4eea1a3de
agumy <nov13sh@gmail.com>
2021-05-02 14:22:34 +0900

    control flow

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -100,3 +100,23 @@ for(key, numbers) in interstingNumbers {
 }
 
 print(largest, largestKey)
+
+var n = 2
+while n < 100 {
+    n *= 2
+}
+print(n)
+
+var m = 2
+repeat {
+    m *= 2
+} while m < 100
+
+print(m)
+
+var total = 0
+for i in 0..<4 {
+    total += i
+}
+
+print(total)
