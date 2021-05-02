03045fe9c8e60acd24e54f70d66b206603ded105
agumy <nov13sh@gmail.com>
2021-05-02 14:43:02 +0900

    function and clojure

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -120,3 +120,93 @@ for i in 0..<4 {
 }
 
 print(total)
+
+func greet(person: String, day: String) -> String {
+    return "Hello \(person), today is \(day)."
+}
+
+greet(person: "Bob", day: "Thurseday")
+
+func greet2(person: String, lunch: String) -> String {
+    return "Hello \(person), today's lunch is \(lunch)."
+}
+
+greet2(person: "Bob", lunch: "Pasta")
+
+func greet3(_ person: String, on day: String) -> String {
+    return "Hello \(person), today is \(day)."
+}
+
+greet3("Bob", on: "Thurseday")
+
+func calcStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+    }
+    
+    return (min, max, sum)
+}
+
+let statistics = calcStatistics(scores: [5, 3, 100, 3, 9])
+print(statistics.sum)
+print(statistics.2)
+
+func returnFifteen() -> Int {
+    var y = 10
+    func add() {
+        y += 5
+    }
+    add()
+    return y
+}
+
+returnFifteen()
+
+func makeIncrementer() -> ((Int) -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
+    }
+    return addOne
+}
+
+var increment = makeIncrementer()
+increment(7)
+
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+          return true
+        }
+    }
+    return false
+}
+
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+
+var numbers = [20, 19, 7, 12]
+
+hasAnyMatches(list: numbers, condition: lessThanTen)
+
+numbers.map({ (number: Int) -> Int in
+    if number % 2 != 0 {
+      return 0
+    }
+    return number
+})
+
+let mappedNumbers = numbers.map({ number in 3 * number })
+print(mappedNumbers)
+
+let sortedNumbers = numbers.sorted { $0 > $1 }
+print(sortedNumbers)
